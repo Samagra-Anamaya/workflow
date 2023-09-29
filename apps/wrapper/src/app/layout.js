eff312700ba092c56f4c10aeca26d5dd7339f269
amit-s19 <coutamit.s19@gmail.com>
2023-09-29 15:56:35 +0530

    addded firebase

--- apps/wrapper/src/app/layout.js
+++ apps/wrapper/src/app/layout.js
@@ -8,6 +8,8 @@ import { OfflineSyncProvider } from 'offline-sync-handler';
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { analytics } from './services/firebase/firebase';
+import { logEvent } from "firebase/analytics";
 
 export default function RootLayout({ children }) {
   const [hydrated, setHydrated] = useState(false);
@@ -52,6 +54,7 @@ export default function RootLayout({ children }) {
       });
     })
     setHydrated(true);
+    logEvent(analytics, 'Test Logger');
   }, [])
 
   return (
