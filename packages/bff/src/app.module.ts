8ca79d8d33bec0426df7682729c161d738053f87
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2023-10-09 15:02:07 +0530

    feat: search by enumeratorid added

--- packages/bff/src/app.module.ts
+++ packages/bff/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { SubmissionController } from './submission/submission.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { EnumeratorModule } from './enumerator/enumerator.module';
+//import { EnumeratorModule } from './enumerator/enumerator.module';
 import { SubmissionService } from './submission/submission.service';
 import { ScheduleModule } from './schedule/schedule.module';
 import { UtilsModule } from './utils/utils.module';
@@ -17,12 +17,11 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 @Module({
   imports: [
     PrismaModule,
-    EnumeratorModule,
+    // EnumeratorModule,
     ScheduleModule,
     UtilsModule,
     EventEmitterModule.forRoot({
       maxListeners: 50,
-      // show event name in memory leak message when more than maximum amount of listeners is assigned
       verboseMemoryLeak: true,
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
