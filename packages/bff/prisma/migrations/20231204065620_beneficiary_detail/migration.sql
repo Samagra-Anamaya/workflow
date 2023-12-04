9cab8ed6ced949e0fd967ee34d9d0ea7286b80d6
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2023-12-04 12:27:12 +0530

    feat: add beneficiary_detail in scheme.prisma

---
+++ packages/bff/prisma/migrations/20231204065620_beneficiary_detail/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "beneficiary_detail" (
+    "id" SERIAL NOT NULL,
+    "aadhaarVaultReference" TEXT NOT NULL,
+    "beneficiaryName" TEXT NOT NULL,
+    "beneficiaryFatherName" TEXT NOT NULL,
+    "beneficiaryMotherName" TEXT NOT NULL,
+    "beneficiarySpouseName" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "dateOfBirth" TEXT NOT NULL,
+    "socialCategory" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "districtName" TEXT NOT NULL,
+    "districtLGDCode" TEXT NOT NULL,
+    "blockName" TEXT NOT NULL,
+    "blockLGDCode" TEXT NOT NULL,
+    "gpName" TEXT NOT NULL,
+    "gpLGDCode" TEXT NOT NULL,
+    "villageName" TEXT NOT NULL,
+    "villageLGDCode" TEXT NOT NULL,
+    "isDisable" TEXT NOT NULL,
+    "maritalStatus" TEXT NOT NULL,
+    "educationalQualification" TEXT NOT NULL,
+    "spdpMemberId" TEXT NOT NULL,
+    "numberOfMemberInFamily" TEXT NOT NULL,
+    "rationCardNumber" TEXT NOT NULL,
+    "primarySourceOfLivelihood" TEXT NOT NULL,
+    "spdpFamilyId" TEXT NOT NULL,
+
+    CONSTRAINT "beneficiary_detail_pkey" PRIMARY KEY ("id")
+);
